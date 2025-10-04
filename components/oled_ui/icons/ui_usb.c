956e2753eb16ed1e16bc844e30ab94b46c47269c
metaneutrons <436979+metaneutrons@users.noreply.github.com>
2025-10-04 08:16:37 +0200

    refactor: use XBM bitmap for USB icon

--- components/oled_ui/icons/ui_usb.c
+++ components/oled_ui/icons/ui_usb.c
@@ -4,14 +4,11 @@
 
 extern u8g2_t u8g2;
 
+// USB XBM bitmap (14x7)
+static const unsigned char usb_icon[] = {
+    0xfe, 0xdf, 0x01, 0xe0, 0x01, 0xe0, 0xfd, 0xef, 0x01, 0xe0, 0x01, 0xe0, 0xfe, 0xdf};
+
 void ui_usb_draw_at(int x, int y)
 {
-    // USB-C connector with rounded corners (1px gaps for rounding)
-    u8g2_DrawHLine(&u8g2, x + 1, y, USB_ICON_WIDTH - 2);     // Top edge
-    u8g2_DrawHLine(&u8g2, x + 1, y + 4, USB_ICON_WIDTH - 2); // Bottom edge
-    u8g2_DrawVLine(&u8g2, x, y + 1, 3);                      // Left edge
-    u8g2_DrawVLine(&u8g2, x + USB_ICON_WIDTH - 1, y + 1, 3); // Right edge
-
-    // Contact strip
-    u8g2_DrawHLine(&u8g2, x + 2, y + 2, USB_ICON_WIDTH - 4);
+    u8g2_DrawXBM(&u8g2, x, y, 14, 7, usb_icon);
 }
