2c25c3ed77c4fe272d1625069c30d1ebb2ec9e28
metaneutrons <436979+metaneutrons@users.noreply.github.com>
2025-10-16 00:01:01 +0200

    chore(ble): add logging for unhandled GATTS events
    - Log all unhandled GATTS events to diagnose connection timeout
    - Helps identify missing event handlers (MTU, notifications, etc)
    - Debug level logging to avoid spam

--- components/bluetooth_config/bluetooth_config.c
+++ components/bluetooth_config/bluetooth_config.c
@@ -370,6 +370,7 @@ static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_
         break;
 
     default:
+        ESP_LOGD(TAG, "UART GATTS event: %d (gatts_if=%d)", event, gatts_if);
         break;
     }
 }
