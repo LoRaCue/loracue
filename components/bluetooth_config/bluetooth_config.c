f4e6a65235fe52ebc8712c07a601f7de1dfff742
metaneutrons <436979+metaneutrons@users.noreply.github.com>
2025-10-16 08:32:07 +0200

    fix(ble): remove const from bluetooth_config_get_passkey parameter
    - Parameter is output, not input - should not be const
    - Fixes compilation error with conflicting types

--- components/bluetooth_config/bluetooth_config.c
+++ components/bluetooth_config/bluetooth_config.c
@@ -29,7 +29,7 @@ bool bluetooth_config_is_connected(void)
     return false;
 }
 
-bool bluetooth_config_get_passkey(const uint32_t *passkey)
+bool bluetooth_config_get_passkey(uint32_t *passkey)
 {
     (void)passkey;
     return false;
