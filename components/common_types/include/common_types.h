552fbcc2e0efbf24a5c6226bed5396a09a3c2414
metaneutrons <436979+metaneutrons@users.noreply.github.com>
2025-11-02 20:18:44 +0100

    chore: update component dependencies and minor fixes
    - Update common_types for new event structures
    - Power management adjustments
    - USB HID CMakeLists.txt updates

--- components/common_types/include/common_types.h
+++ components/common_types/include/common_types.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -12,6 +14,18 @@ typedef enum {
     BUTTON_EVENT_DOUBLE,    // 2 clicks <200ms apart
     BUTTON_EVENT_LONG       // >2s
 } button_event_type_t;
+
+/**
+ * @brief Command history entry for PC mode
+ */
+typedef struct {
+    uint32_t timestamp_ms;
+    uint16_t device_id;
+    char device_name[16];
+    char command[8];
+    uint8_t keycode;
+    uint8_t modifiers;
+} command_history_entry_t;
 
 #ifdef __cplusplus
 }
